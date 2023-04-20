4d12ce2f59c8c9d4f3866fb88e6d5933a70528b5
Sleekstiv007 <smiene13@gmail.com>
2023-04-20 14:33:14 +0100

    up-to-date

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,25 +1,24 @@
 import React from "react";
-import { categories } from "../data/data";
+import { categories } from "../data/data.js";
 
 const Category = () => {
   // console.log(categories);
   return (
-    <div className="max-w-[1640px] m-auto px-4 py-12">
+    <div className="max-w-[1640] mx-auto px-4 py-12">
       <h1 className="text-orange-600 font-bold text-4xl text-center">
         Top Rated Menu Items
       </h1>
-      {/* Categories */}
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6 ">
+      {/* CATEGORIES */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-6 py-6">
         {categories.map((item, index) => (
-          <div
-            key={index}
-            className="bg-gray-100 rounded-lg p-4 flex justify-between items-center"
-          >
-            <h2 className="font-bold mr-2 text-sm sm:text-xl">{item.name}</h2>
+          <div className="bg-gray-100 rounded-lg p-4 text-center " key={index}>
+            <h2 className="font-bold flex justify-center items-center sm:text-xl">
+              {item.name}
+            </h2>
             <img
+              className="w-[400px] md:w-[300px] h-[200px] object-cover "
               src={item.image}
               alt={item.name}
-              className="w-20 h-[80px] object-cover rounded-t-lg"
             />
           </div>
         ))}
